f1696173e031189b8c9871ee2e3c2330d08886cd
E. Redondo <eduardo.redondo@univ-eiffel.fr>
2022-10-27 11:55:17 +0200

    added func2str_struct and str2func_struct (read/write_json_struct dependencies)

---
+++ data_tools/func2str_struct.m
@@ -0,0 +1,16 @@
+function struct_out = func2str_struct(struct_in)
+%convert function handles to strings
+fieldlist = fieldnames(struct_in);
+
+for ind = 1:length(struct_in)
+    for ind_f = 1:length(fieldlist)
+        field = struct_in(ind).(fieldlist{ind_f});
+        if isstruct(field)
+            field = func2str_struct(field);
+        elseif isa(field,'function_handle')
+            field = ['@' func2str(field)];
+        end
+        struct_out(ind).(fieldlist{ind_f}) = field;
+    end
+end
+end
---
+++ data_tools/str2func_struct.m
@@ -0,0 +1,18 @@
+function struct_out = str2func_struct(struct_in)
+%convert strings to function handles
+fieldlist = fieldnames(struct_in);
+
+for ind = 1:length(struct_in)
+    for ind_f = 1:length(fieldlist)
+        field = struct_in(ind).(fieldlist{ind_f});
+        if isstruct(field)
+            field = str2func_struct(field);
+        elseif ischar(field) && length(field)>1
+            if field(1)=='@' && isvarname(field(2:end))
+                field = str2func(field);
+            end
+        end
+        struct_out(ind).(fieldlist{ind_f}) = field;
+    end
+end
+end
