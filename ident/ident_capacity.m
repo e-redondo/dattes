04e79827f03c95855792f7b894fdffac54aa08cf
E. Redondo <eduardo.redondo@univ-eiffel.fr>
2022-10-29 17:46:04 +0200

    ident_capacity: changed some names of cccv results (cc_cv to cccv for all); cccv_ratio_cc_ah and cccv_ratio_cc_duration are now ratios (i.e. part of CC / total, in p.u.) with same size as other cccv results (1xn elements)

--- ident/ident_capacity.m
+++ ident/ident_capacity.m
@@ -24,11 +24,11 @@ function [capacity] = ident_capacity(config,phases,options)
 %     - cv_voltage (1xj) double: voltage of each CV phase
 %     - cv_time (1xj) double: final time of each CV phase
 %     - cv_duration (1xj) double: duration of each CV phase
-%     - cc_cv_time (1xn) double: final time of cc part of each CC-CV capacity measurement
-%     - cc_cv_capacity (1xn) double: sum of CC and CV capacity measurements
-%     - cc_cv_duration(1xn) double: sum of CC and CV capacity durations
-%     - cccv_ratio_cc_ah (nx2) double : CC / CV part of capacity measurements (AmpHours) 
-%     - cccv_ratio_cc_duration (nx2) double : duration of  CC / CV part of capacity measurements 
+%     - cccv_time (1xn) double: final time of cc part of each CC-CV capacity measurement
+%     - cccv_capacity (1xn) double: sum of CC and CV capacity measurements
+%     - cccv_duration(1xn) double: sum of CC and CV capacity durations
+%     - cccv_ratio_cc_ah (1xn) double : CC part of CCCV capacity measurements (p.u. of total capacity) 
+%     - cccv_ratio_cc_duration (1xn) double : CC part of CCCV capacity measurements duration (p.u. of CCCV duration) 
 %
 % See also dattes, split_phases, configurator, plot_capacity
 %
@@ -86,7 +86,7 @@ cv_duration = [phases_cv.duration];
 
 %CC+CV
 
-[cc_cv_time,cc_cv_capacity,cc_cv_duration,cc_cv_crate,cccv_ratio_cc_ah,cccv_ratio_cc_duration]=append_cc_and_cv(config,phases);
+[cccv_time,cccv_capacity,cccv_duration,cccv_crate,cccv_ratio_cc_ah,cccv_ratio_cc_duration]=append_cc_and_cv(config,phases);
 
 %put into output structure:
 
@@ -100,10 +100,10 @@ capacity.cv_voltage = cv_voltage;
 capacity.cv_time = cv_time;
 capacity.cv_duration = cv_duration;
 
-capacity.cc_cv_time=cc_cv_time;
-capacity.cc_cv_capacity=cc_cv_capacity;
-capacity.cc_cv_duration=cc_cv_duration;
-capacity.cc_cv_crate=cc_cv_crate;
+capacity.cccv_time=cccv_time;
+capacity.cccv_capacity=cccv_capacity;
+capacity.cccv_duration=cccv_duration;
+capacity.cccv_crate=cccv_crate;
 
 capacity.cccv_ratio_cc_ah=cccv_ratio_cc_ah;
 capacity.cccv_ratio_cc_duration=cccv_ratio_cc_duration;
@@ -115,18 +115,18 @@ end
 end
 
 
-function [cc_cv_time,cc_cv_capacity,cc_cv_duration, cc_cv_crate,cccv_ratio_cc_ah,cccv_ratio_cc_duration]=append_cc_and_cv(config,phases)
+function [cccv_time,cccv_capacity,cccv_duration, cccv_crate,cccv_ratio_cc_ah,cccv_ratio_cc_duration]=append_cc_and_cv(config,phases)
 
 
 
 
 
 %Look for cc capacity phases followed by cv capacity phases
-phases_cc_cv=config.capacity.pCapaC |config.capacity.pCapaCV| config.capacity.pCapaD  | config.capacity.pCapaDV ;
+phases_cccv=config.capacity.pCapaC |config.capacity.pCapaCV| config.capacity.pCapaD  | config.capacity.pCapaDV ;
 sequence=[1 1];
 
 %Store position of cc phases which are followed by cv phases
-Index_cc_cv=strfind(phases_cc_cv, sequence);
+Index_cccv=strfind(phases_cccv, sequence);
 
 %Calculate full capacity at these positions 
 capacities=[phases.capacity];
@@ -136,41 +136,42 @@ crates = [phases.Iavg]./config.test.capacity;
 
 %Store the (dis)charged Ah and duration in arrays for each CC-CV phases
 
-cc_cv_capacity=nan(1,length(Index_cc_cv));
-cc_cv_duration=nan(1,length(Index_cc_cv));
-cc_cv_time=nan(1,length(Index_cc_cv));
-ah_cc_in_cc_cv=nan(1,length(Index_cc_cv));
-ah_cv_in_cc_cv=nan(1,length(Index_cc_cv));
-duration_cc_in_cc_cv=nan(1,length(Index_cc_cv));
-duration_cv_in_cc_cv=nan(1,length(Index_cc_cv));
-cc_cv_crate=nan(1,length(Index_cc_cv));
+cccv_capacity=nan(1,length(Index_cccv));
+cccv_duration=nan(1,length(Index_cccv));
+cccv_time=nan(1,length(Index_cccv));
+ah_cc_in_cccv=nan(1,length(Index_cccv));
+ah_cv_in_cccv=nan(1,length(Index_cccv));
+duration_cc_in_cccv=nan(1,length(Index_cccv));
+duration_cv_in_cccv=nan(1,length(Index_cccv));
+cccv_crate=nan(1,length(Index_cccv));
 
-for indice=1:length(Index_cc_cv)
-    cc_cv_time(indice)=times(Index_cc_cv(1,indice));
-    cc_cv_capacity(indice)= capacities(Index_cc_cv(1,indice))+capacities(Index_cc_cv(1,indice)+1);
+for indice=1:length(Index_cccv)
+    cccv_time(indice)=times(Index_cccv(1,indice));
+    cccv_capacity(indice)= capacities(Index_cccv(1,indice))+capacities(Index_cccv(1,indice)+1);
     
-    cc_cv_duration(indice)=durations(Index_cc_cv(1,indice))+durations(Index_cc_cv(1,indice)+1);
-    cc_cv_crate(indice)=crates(Index_cc_cv(1,indice));
+    cccv_duration(indice)=durations(Index_cccv(1,indice))+durations(Index_cccv(1,indice)+1);
+    cccv_crate(indice)=crates(Index_cccv(1,indice));
 end
 
 
 %Calculate and store the ratio of (dis)charged Ah and duration in CC and CV phases
 %over (dis)charged Ah and duration during the CC-CV phases
     
-for indice=1:length(Index_cc_cv)
+for indice=1:length(Index_cccv)
 
-    ah_cc_in_cc_cv(indice)=capacities(Index_cc_cv(1,indice));
-    ah_cv_in_cc_cv(indice)=capacities(Index_cc_cv(1,indice)+1);
+    ah_cc_in_cccv(indice)=capacities(Index_cccv(1,indice));
+    ah_cv_in_cccv(indice)=capacities(Index_cccv(1,indice)+1);
       
     
-    duration_cc_in_cc_cv(indice)= durations(Index_cc_cv(1,indice));
-    duration_cv_in_cc_cv(indice)= durations(Index_cc_cv(1,indice)+1);  
+    duration_cc_in_cccv(indice)= durations(Index_cccv(1,indice));
+    duration_cv_in_cccv(indice)= durations(Index_cccv(1,indice)+1);  
 end
 
+% 
+% cccv_ratio_cc_ah=[ah_cc_in_cccv; ah_cv_in_cccv]';
+% cccv_ratio_cc_duration=[duration_cc_in_cccv; duration_cv_in_cccv]';
 
-cccv_ratio_cc_ah=[ah_cc_in_cc_cv; ah_cv_in_cc_cv]';
-cccv_ratio_cc_duration=[duration_cc_in_cc_cv; duration_cv_in_cc_cv]';
-
-
+cccv_ratio_cc_ah=ah_cc_in_cccv./cccv_capacity;
+cccv_ratio_cc_duration=duration_cc_in_cccv./cccv_duration;
 
 end
