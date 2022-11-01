97ffefb3ecd19ff06a4c6e0a669ba246f44fc132
E. Redondo <eduardo.redondo@univ-eiffel.fr>
2022-11-01 13:16:05 +0100

    result_filename: added possibility to store mat file in other folder than xml file, added custom mat file suffix (default: "dattes")

--- results/result_filename.m
+++ results/result_filename.m
@@ -1,29 +1,51 @@
-function fileOut = result_filename(XMLfile)
-%result_filename Create result file named xml_file_result.mat
+function file_out = result_filename(xml_file, dst_folder,suffix)
+%result_filename Create result file named xml_file_suffix.mat
 %
-%  fileOut = result_filename(XMLfile)
-% Create result file named xml_file_result.mat
+% Create result file named xml_file_suffix.mat
 %
 % Usage:
 % [result, config] = edit_result(result, config,Field,Values,options)
 % Inputs : 
-% - xml_file:
-%     -   [1xn string]: pathame to the xml file
-%     -   [nx1 cell string]: xml filelist
-% Outputs : 
-% - fileOut: [string] full file name of the result file
+% - xml_file [1xn string]: pathame to the xml file
+% - dst_folder [1xp string]: (optional) destinaiton folder (default= src_folder)
+% - suffix [1xq string]: (optional) filename suffix (default= 'dattes')
 %
-% See also dattes, load_result, edit_result
+% Outputs : 
+% - xml_file: [string] full file name of the result file
+%
+% Examples:
+% (1) file_out = result_filename(raw_data/test.xml)
+%       file_out = 'raw_data/test_dattes.mat'
+% (2) file_out = result_filename(raw_data/test.xml,'processed_data')
+%       file_out = 'processed_data/test_dattes.mat'
+% (3) file_out = result_filename(raw_data/test.xml,'','analysed')
+%       file_out = 'raw_data/test_analysed.mat'
+% (4) file_out = result_filename(raw_data/test.xml,'','')
+%       file_out = 'raw_data/test.mat'
+%
+% See also dattes, load_result, save_result
 %
 % Copyright 2015 DATTES_Contributors <dattes@univ-eiffel.fr> .
 % For more information, see the <a href="matlab: 
 % web('https://gitlab.com/dattes/dattes/-/blob/main/LICENSE')">DATTES License</a>.
 
+if ~exist('suffix','var')
+    suffix = 'dattes';
+end
 
 %separate folder (D), file (F) and extension (E)
-[D F E] = fileparts(XMLfile);
+[src_folder, F, E] = fileparts(xml_file);
+
 %print suffix: filename_result.mat
-fileOut = sprintf('%s_dattes.mat',F);
+if isempty(suffix)
+    file_out = sprintf('%s.mat',F);
+else
+    file_out = sprintf('%s_%s.mat',F,suffix);
+end
 %build the full pathname (folder + filename)
-fileOut = fullfile(D, fileOut);
+if isempty(dst_folder)
+    file_out = fullfile(src_folder, file_out);
+else
+    file_out = fullfile(dst_folder, file_out);
+end
 end
