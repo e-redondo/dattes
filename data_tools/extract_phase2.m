a2a92c5263a8a39002326735ffa0960d71c71a61
E. Redondo <eduardo.redondo@univ-eiffel.fr>
2023-01-24 01:24:19 +0100

    extract_phase2: now accept profiles as input to cut all its vectors, in that case profiles_p is returned

--- data_tools/extract_phase2.m
+++ data_tools/extract_phase2.m
@@ -2,22 +2,38 @@ function [date_time_p,varargout] = extract_phase2(phases,t_be_af,date_time,varar
 % extract_phase2 extract vectors for given phases with additional
 % parameters
 %
-% Usage:
+% Usage (1):
 % [date_time_p,Up,Ip,mp...] = extract_phase2(phases,t_be_af,date_time,U,I,m...)
 %
-% Inputs:
+% Inputs (1):
 % - phases (mx1 struct): phase struct array from split_phases
 % - t_be_af (1x2 double): time before and after
 %     - t_be_af(1): seconds before phases(1).datetime_ini to add to outputs
 %     - t_be_af(2): seconds after phases(end).datetime_ini to add to outputs
 % - date_time, U, I, m ... (nx1 double): vectors from extract_profiles
 %
-% Outputs:
+% Outputs (1):
 % - date_time_p, Up, Ip, mp ... (px1 double): subvectors corresponding to phase
 %
-% Example: [tp,Up,Ip] = extract_phase2(phases(3:5),[30 45],t,U,I)
+% Usage (2):
+% [profiles_p] = extract_phase2(phases,t_be_af,profiles...)
+%
+% Inputs (2):
+% - phases (mx1 struct): phase struct array from split_phases
+% - t_be_af (1x2 double): time before and after
+%     - t_be_af(1): seconds before phases(1).datetime_ini to add to outputs
+%     - t_be_af(2): seconds after phases(end).datetime_ini to add to outputs
+% - profiles (1x1 struct): profiles struct
+%
+% Outputs:
+% - profiles_p (1x1 struct): profiles struct with cutted vectors
+%
+% Example (1): [tp,Up,Ip] = extract_phase2(phases(3:5),[30 45],t,U,I)
 % takes 30 seconds before beginning of phase(3) and 45 seconds after the
 % end of phase(5).
+%
+% Example (2): [profiles_p] = extract_phase2(phases(3:5),[30 45],profiles)
+% the same, but with profiles sub structure
 %
 % See also extract_phase, split_phases, extract_profiles
 %
@@ -53,21 +69,38 @@ if ~isfield(phases,'datetime_ini') || ~isfield(phases,'datetime_fin')
     fprintf('extract_phase2: ERREUR, phases doit avoir des champs datetime_ini et datetime_fin\n');
     return;
 end
-%0.6.- t doit etre vecteur double
-if ~isa(date_time,'double')
-    fprintf('extract_phase2: ERREUR, le vecteur temps doit etre un double\n');
+%0.6.- surcharge de function: date_time struct >>> profiles as input
+if isa(date_time,'struct')
+    profiles = date_time;
+    date_time = profiles.datetime;
+
+
+    indices = date_time>=phases(1).datetime_ini-t_be_af(1) & date_time<=phases(end).datetime_fin+t_be_af(2);
+    
+    field_list = fieldnames(profiles);
+    for ind = 1:length(field_list)
+        profiles_p.(field_list{ind}) = profiles.(field_list{ind})(indices);
+    end
+    %put profiles_p as date_time_p to be returned as main funciton output
+    date_time_p = profiles_p;
+
+    %0.6.- date_time doit etre vecteur double
+elseif isa(date_time,'double')
+    %0.7.-varargin doivent etre des vecteurs de la meme taille que 't'
+    for ind = 1:length(varargin)
+        if ~isequal(size(varargin{ind}),size(date_time))
+            fprintf('extract_phase2: ERREUR, dans les entrees tous les vecteurs n''ont pas a meme taille\n');
+            return;
+        end
+    end
+
+    indices = date_time>=phases(1).datetime_ini-t_be_af(1) & date_time<=phases(end).datetime_fin+t_be_af(2);
+    date_time_p = date_time(indices);
+    varargout = cellfun(@(x) x(indices),varargin,'uniformoutput',false);
+else
+    fprintf('extract_phase2: ERROR, datetime vector must be numeric\n');
     return;
 end
-%0.7.-varargin doivent etre des vecteurs de la meme taille que 't'
-for ind = 1:length(varargin)
-    if ~isequal(size(varargin{ind}),size(date_time))
-        fprintf('extract_phase2: ERREUR, dans les entrees tous les vecteurs n''ont pas a meme taille\n');
-        return;
-    end
-end
 
-indices = date_time>=phases(1).datetime_ini-t_be_af(1) & date_time<=phases(end).datetime_fin+t_be_af(2);
-date_time_p = date_time(indices);
-varargout = cellfun(@(x) x(indices),varargin,'uniformoutput',false);
 
 end
