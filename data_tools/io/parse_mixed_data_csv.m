36dfdcf2db0acd6ca56e324c58418e60a8099107
E. Redondo <eduardo.redondo@univ-eiffel.fr>
2023-02-06 18:17:31 +0100

    rewriting of parse_mixed_data_csv, now using textscan much faster than previous version

--- data_tools/io/parse_mixed_data_csv.m
+++ data_tools/io/parse_mixed_data_csv.m
@@ -1,4 +1,23 @@
-function [header_lines,data_columns,tail_lines,data_lines] = parse_mixed_data_csv(file_in,params)
+function [header_lines,data_columns,tail_lines] = parse_mixed_data_csv(file_in,params)
+% parse_mixed_data_csv - Read CSV files with mixed data (numeric/text),
+% with headers and trailing lines after data.
+%
+% Usage: [header_lines,data_columns,tail_lines] = parse_mixed_data_csv(file_in,params)
+%
+% Inputs:
+% - file_in [char]: pathname to the csv file
+% - paramas [struct] with fields:
+%   - 'col_sep' [char]: column separator (default ',')
+%   - 'dec_sep' [char]: decimal separator (default '.'), (not yet used)
+%   - 'str_sep' [char]: decimal separator (default ''), (not yet used)
+
+%TODO: treat emptys as text instead as numbers
+% Currently if empty value is found in text_line, '%f'.
+% This is OK for Arbin files: some columns are sparse numeric (most of
+% empty, some numbers), but it does not work with Bitrode files: some
+% columns are mostly empty but non empty values are char. This makes
+% textscan to stop.
+
 
 if ~exist('params','var')
     params = struct;
@@ -12,79 +31,84 @@ if ~isfield(params,'dec_sep')
 end
 if ~isfield(params,'str_sep')
     params.str_sep = '';
-    %params.str_sep = '"'; % to replace " by nothing
-    %params.str_sep = "'"; % to replace ' by nothing
+    %params.str_sep = '"'; % to ignore "
+    %params.str_sep = "'"; % to ignore '
 
 end
 
+%1. read header
 fid = fopen(file_in);
 [header_lines,fid] = read_csv_header(fid,params);
 
-data_lines = header_lines(end);
+first_data_line = header_lines{end};
 header_lines = header_lines(1:end-1);
-while ~feof(fid)
-    data_lines{end+1,1} = fgetl(fid);
-end
-fclose(fid);
+%2. build text_fmt
+text_fmt = build_scanf_fmt(first_data_line,params);
 
-%clean 'str_sep':
-if ~isempty(params.str_sep)
-    %TODO: just replace by couple of str_sep "protect string"
-    %TODO: how to protect from split, e.g. "string1","string2, with ,"
-    data_lines = regexprep(data_lines,['\' params.str_sep],'');
-end
-
-%cut lines into words:
-data_matrix = regexp(data_lines,params.col_sep,'split');
+%3. textscan
+data_columns1 = textscan(first_data_line,text_fmt,'Delimiter',params.col_sep,'Whitespace',[' \b\t' params.str_sep]);
+data_columns = textscan(fid,text_fmt,'Delimiter',params.col_sep,'Whitespace',[' \b\t' params.str_sep]);
 
 
-nr_words = cellfun(@length,data_matrix);
-ind_tail_lines = find(nr_words~=nr_words(1),1);
+data_columns = cellfun(@(x,y) vertcat(x,y),data_columns1,data_columns,'UniformOutput',false);
 
-if ~isempty(ind_tail_lines)
-    tail_lines = data_lines(ind_tail_lines:end);
-    data_lines = data_lines(1:ind_tail_lines-1);
-    data_matrix = data_matrix(1:ind_tail_lines-1);
-else
-    tail_lines = cell(0);
-end
-data_matrix = vertcat(data_matrix{:});
+data_columns = data_columns';
 
-%find column types from ifirst line of data:
-this_line = data_lines{1};
-this_words = regexp(this_line,params.col_sep,'split');
-index_numbers = cellfun(@str2num,this_words,'UniformOutput',false);
-index_numbers = ~cellfun(@isempty,index_numbers);
-
-%expression = '^-{0,1}[0-9]*\.{0,1}[0-9]*((e|E)(+|-)[0-9]*){0,1}$'
-expression = '^\-{0,1}[0-9]*\.{0,1}[0-9]+([eE][+-]{0,1}[0-9]+){0,1}$';
-index_numbers = regexp(this_words,expression,'match','once');
-index_numbers = ~cellfun(@isempty,index_numbers);
-
-% index_dates = ~cellfun(@isempty,regexp(this_words,'^[0-9]*[^0-9\.\-].*[0-9AP][0-9M]$','match','once'));
-%for each column do conversions:
-data_columns = cell(size(data_matrix,2),1);
-for ind = 1:size(data_matrix,2)
-    this_column = data_matrix(:,ind);
-    if index_numbers(ind)
-        %replace empties with nans
-        ind_empty = cellfun(@isempty,this_column);
-        this_column(ind_empty) = deal({'nan'});
-        %this column is numeric
-%         data_matrix(:,ind) = num2cell(str2double(this_column));
-%         data_matrix(:,ind) =  cellfun(@(x) sscanf(x,'%f'),this_column,'UniformOutput',false);
-        data_columns{ind} = cellfun(@(x) sscanf(x,'%f'),this_column);
-%     elseif index_dates(ind)
-        %this column is a date
-%         try
-%           data_matrix(:,ind) = num2cell(datenum(this_column));
-%         catch
-            %do nothing
-%         end
-    else
-        data_columns{ind} = this_column;
+%3.1 replace each char in str_sep by nothing in string columns:
+for ind = 1:length(data_columns)
+    if iscell(data_columns{ind})
+        for ind2 = 1:length(params.str_sep)
+            data_columns{ind} = strrep(data_columns{ind},params.str_sep(ind2),'');
+        end
     end
 end
+
+%4.tail_lines
+tail_lines = cell(0);
+while ~feof(fid)
+    tail_lines{end+1} = fgetl(fid); 
+end
+
+fclose(fid);
+end
+
+function text_fmt = build_scanf_fmt(text_line,params)
+% search in text line numeric data, then build a format string for textscan
+
+if ~exist('params','var')
+    params = struct;
+end
+
+if ~isfield(params,'col_sep')
+    params.col_sep = ',';
+end
+
+if ~isfield(params,'str_sep')
+    params.str_sep = '';
+    %params.str_sep = '"'; % to ignore "
+    %params.str_sep = "'"; % to ignore '
+    
+end
+
+%cut line into 'words'
+words = regexp(text_line,params.col_sep,'split');
+%trim leading and trialing spaces
+words = strtrim(words);
+%ignore some characters:
+for ind =1:length(params.str_sep)
+words = strrep(words,params.str_sep(ind),'');
+end
+%build regex expression to detect numbers
+expression = '^\-{0,1}[0-9]*\.{0,1}[0-9]+([eE][+-]{0,1}[0-9]+){0,1}$';
+%find 'numbers'
+numbers = regexp(words,expression,'match','once');
+%check back where 'numbers' are equal to 'words'
+%index_numbers is thus, positions where numbers are
+index_numbers = cellfun(@(x,y) isequal(x,y),words,numbers);
+
+text_fmt = repmat({'%s'},size(words));
+text_fmt(index_numbers) = deal({'%f'});
+text_fmt = strjoin(text_fmt,' ');
 
 
 end
