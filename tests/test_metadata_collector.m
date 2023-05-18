dc64e4c148021556d0850c8eafa7b6e17e35e232
E. Redondo <eduardo.redondo@univ-eiffel.fr>
2023-05-18 15:18:52 +0200

    removed tests folder, test will be developed in a different repository

--- tests/test_metadata_collector.m
+++
@@ -1,5 +0,0 @@
-srcdir = '/home/redondo/essais/DATTES_test_data/0_original_data/test_metadata_collector';
-
-csv_list = lsFiles(srcdir,'.csv');
-
-[metadata, meta_list,errors] = cellfun(@metadata_collector,csv_list,'UniformOutput',false);
